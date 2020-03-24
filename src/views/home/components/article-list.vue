5a442e6920e5b2510d3aab74df6129a2e4457072
liuxiandi <liuxiandi@example.com>
2020-03-25 00:09:44 +0800

    article-list实现下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -3,11 +3,15 @@
   <!-- van-list 可以帮助我们实现上拉加载  需要一些变量 -->
   <!-- 这里放置div的目的是形成滚动条，后期我们要做阅读记忆 -->
   <div class="scroll-wrapper">
-      <van-list finished-text='没有了' v-model="upLoading" :finished="finished" @load="onLoad">
-          <van-cell-group>
-              <van-cell v-for="item in articles" :key="item" title="美股又熔断了" :value="'天台排队' + item"></van-cell>
-          </van-cell-group>
+     <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="successText">
+      <van-list finished-text="没有了" v-model="upLoading" :finished="finished" @load="onLoad">
+        <!-- 循环内容 -->
+        <van-cell-group>
+          <van-cell v-for="item in articles" :key="item" title="美股又熔断了" :value="'天台排队'+item"></van-cell>
+        </van-cell-group>
       </van-list>
+    </van-pull-refresh>
+
   </div>
 </template>
 
@@ -19,7 +23,9 @@ export default {
       upLoading: false,
       // 表示是否已经完成所有数据的加载
       finished: false,
-      articles: []
+      articles: [],
+      successText: '',
+      downLoading: false
     }
   },
   methods: {
@@ -41,7 +47,19 @@ export default {
     //     // 表示数据已经全部加载完毕 没有数据了
     //     this.finished = true
     //   }, 1000)
+    },
+    onRefresh () {
+      setTimeout(() => {
+      // 下拉刷新 表示要读取最新的数据 而且最新的数要添加到数据头部
+        const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
+        // 数组添加到头部
+        this.articles.unshift(...arr)
+        // 手动关闭正在加载的状态
+        this.downLoading = false
+        this.successText = `跟新了${arr.length}条数据`
+      }, 1000)
     }
+
   }
 
 }
