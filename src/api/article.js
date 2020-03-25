464a4b402dee885166f3ff4c904e4dd2ac10a536
liuxiandi <liuxiandi@example.com>
2020-03-25 23:05:34 +0800

    文章列表-封装渲染列表方法

---
+++ src/api/article.js
@@ -0,0 +1,8 @@
+// 处理文章模块的请求数据
+import request from '@/utils/request'
+export function getArticle (params) {
+  return request({
+    url: 'http://ttapi.research.itcast.cn/app/v1_1/articles',
+    params: { with_top: 1, ...params }
+  })
+}
