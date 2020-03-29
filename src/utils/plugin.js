a83fd1ba1a848cd99dfb15302d9a624f78b5d3a5
liuxiandi <liuxiandi@example.com>
2020-03-29 23:15:53 +0800

    时间格式处理

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,3 +1,7 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
 export default {
   // 导出一个默认对象
   install (Vue) {
@@ -5,7 +9,9 @@ export default {
     // 执行此行代码时 Vue.prototype.$notify 应该已经挂载完成
     Vue.prototype.$gnotify = (params) => Vue.prototype.$notify({ duration: 800, ...params }) // 小伎俩
     // 给Vue的原型属性赋值一个函数 自定义一个函数名
+    // 定义一个原型属性$sleep 所有组件都有了这个属性
     Vue.prototype.$sleep = sleep
+    Vue.filter('relTime', relTime)
   }
 }
 function sleep (time = 500) {
@@ -13,4 +19,7 @@ function sleep (time = 500) {
   return new Promise(function (resolve, reject) {
     setTimeout(() => resolve(), time)
   })
+}
+function relTime (date) {
+  return dayjs().locale('zh-cn').from(date)
 }
